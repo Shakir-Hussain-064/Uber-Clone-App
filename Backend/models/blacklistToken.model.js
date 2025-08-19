926ea0d5e65c27d363759be5814aff6ed32c4eda
Escape <Escape>
2025-08-19 20:13:42 +0530

    ...

--- Backend/models/blacklistToken.model.js
+++ Backend/models/blacklistToken.model.js
@@ -1,10 +1,5 @@
 const mongoose = require('mongoose');
 
-// Delete the model from the cache if it exists
-if (mongoose.models.BlacklistToken) {
-    delete mongoose.models.BlacklistToken;
-}
-
 const blacklistTokenSchema = new mongoose.Schema({
     token: {
         type: String,
